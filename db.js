de6fc3db552f822590ddf4114bb3fcdfca22e6b2
prannal <pranjalinal06@gmail.com>
2019-12-01 02:20:01 +0530

    init

---
+++ db.js
@@ -0,0 +1,22 @@
+const mysql=require('mysql')
+
+function connect()
+{
+    const connection=mysql.createConnection(
+        {
+            host:'192.168.2.34',
+            user:'root',
+            password:'root',
+            database:'myapp_db',
+           port:9090
+           }
+    )
+
+    connection.connect()
+    return connection
+}
+
+module.exports=
+{
+    connect:connect
+}
